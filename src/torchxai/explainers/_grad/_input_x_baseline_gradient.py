04efc11083c7ef3abe87491c58bb80a56386038c
Saifullah <saifullah.saifullah@dfki.uni-kl.de>
2026-04-01 07:13:53 +0200

    add correct version. add prints.

--- src/torchxai/explainers/_grad/_input_x_baseline_gradient.py
+++ src/torchxai/explainers/_grad/_input_x_baseline_gradient.py
@@ -41,7 +41,7 @@ class MultiTargetInputBaselineXGradient(GradientAttribution):
         multiply_by_inputs: bool = True,
         gradient_func=(
             _compute_gradients_vmap_autograd
-            if torch.__version__ >= "2.3.0"
+            if torch.__version__ >= "2.1.0"
             else _compute_gradients_sequential_autograd
         ),
         grad_batch_size: int = 10,
