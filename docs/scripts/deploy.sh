142c26c1317c52387a3562c16876133ac6af09df
saifullah3396 <saifullah3396@gmail.com>
2026-05-19 16:48:10 +0200

    fix deployment.

--- docs/scripts/deploy.sh
+++ docs/scripts/deploy.sh
@@ -1,1 +1,2 @@
-mkdocs gh-deploy --config-file ./pages/mkdocs.yml --remote-branch docs
+uv run mkdocs build -f pages/mkdocs.yml
+uv run mkdocs gh-deploy --config-file ./pages/mkdocs.yml --remote-branch docs
